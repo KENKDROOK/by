60b59653a663867c794bbe7655749965e73e5b23
KENKDROOK <45662125+KENKDROOK@users.noreply.github.com>
2019-02-12 20:50:38 +0530

    Create no of times.c

---
+++ no of times.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int n,i;
+    printf("Enter the times ");
+    scanf("%d", &n);
+
+   for(i=0;i<n;i++)
+    {
+        printf("\nHELLO");
+    }
+    getch();
+}
